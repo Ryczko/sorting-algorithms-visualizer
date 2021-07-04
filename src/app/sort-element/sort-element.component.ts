4b88e899174c43751279943fac0f7fc8efda03d9
Ryczko <konradryczko1@gmail.com>
2021-07-04 11:50:07 +0200

    Improve interface

--- src/app/sort-element/sort-element.component.ts
+++ src/app/sort-element/sort-element.component.ts
@@ -1,16 +1,21 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { SettingsService } from '../services/settings.service';
 
 @Component({
   selector: 'app-sort-element',
   templateUrl: './sort-element.component.html',
   styleUrls: ['./sort-element.component.scss'],
 })
-export class SortElementComponent implements OnInit {
+export class SortElementComponent {
   @Input() value: number;
 
-  ngOnInit(): void {}
+  constructor(public settingsService: SettingsService) {}
 
   getColorValue() {
     return `hsl(${Math.round(this.value) * 3.6}, 100%, 50%)`;
+  }
+
+  getElementWidth() {
+    return `calc(${100 / this.settingsService.maxValue}% - 3px)`;
   }
 }
